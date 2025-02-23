34b17244960ae47616b9b2ee60eb74cdb9673eaa
cheli-sh <DST4205@GMAIL.COM>
2025-02-23 23:50:31 +0200

    Add files via upload

--- ToDoDbContext.cs
+++ ToDoDbContext.cs
@@ -19,7 +19,7 @@ public partial class ToDoDbContext : DbContext
     public virtual DbSet<Item> Items { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseMySql("name=ToDoDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
+        => optionsBuilder.UseMySql("name=ToDoDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
